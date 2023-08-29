37f53b679a31cd3d7a81cf872617f1c6ef947430
Ryan Schaffer <rys686@usask.ca>
2023-08-29 15:06:30 -0600

    smb share test

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -94,8 +94,8 @@
       # prevent hang on network split
       automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
 
-    in ["${automount_opts},credentials=/etc/nixos/smb-creds.txt,uid=1000,gid=100"]
-  }
+    in ["${automount_opts},credentials=/etc/nixos/smb-creds.txt,uid=1000,gid=100"];
+  };
 
   fonts = {
     fonts = with pkgs; [
