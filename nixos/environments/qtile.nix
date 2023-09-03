6a0cd5a2f7be7c047b969d6909c755a7ba0d1c55
Ryan Schaffer <ryan.schaffer@live.com>
2023-09-02 22:52:39 -0600

    fixed udev rules (maybe) - widget unfunctional.

--- nixos/environments/qtile.nix
+++ nixos/environments/qtile.nix
@@ -1,17 +1,17 @@
-{
+{pkgs, ...}: {
   services.xserver = {
     enable = true;
     layout = "us";
     windowManager.qtile.enable = true;
     windowManager.qtile.extraPackages = p: with p; [qtile-extras];
-    displayManager.startx.enable = true;
+    # displayManager.startx.enable = true;
     displayManager.lightdm.enable = true;
   };
 
   programs.dconf.enable = true;
 
   services.udev.extraRules = ''
-    ACTION=="add", SUBSYSTEM=="backlight", KERNEL=="nvidia_wmi_ex_backlight", RUN+="/bin/chgrp video /sys/class/backlight/%k/brightness"
-    ACTION=="add", SUBSYSTEM=="backlight", KERNEL=="nvidia_wmi_ex_backlight", RUN+="/bin/chmod g+w /sys/class/backlight/%k/brightness"
+    ACTION=="add", SUBSYSTEM=="backlight", KERNEL=="nvidia_wmi_ex_backlight", RUN+="${pkgs.coreutils}/bin/chgrp video /sys/class/backlight/%k/brightness"
+    ACTION=="add", SUBSYSTEM=="backlight", KERNEL=="nvidia_wmi_ex_backlight", RUN+="${pkgs.coreutils}/bin/chmod g+w /sys/class/backlight/%k/brightness"
   '';
 }
