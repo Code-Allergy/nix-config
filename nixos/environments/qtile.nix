c57a858b05f01296ba599fc4961864cfeb10afb4
Ryan Schaffer <rys686@usask.ca>
2023-09-02 22:40:38 -0600

    added additional udev rule for QTile monitor widget

--- nixos/environments/qtile.nix
+++ nixos/environments/qtile.nix
@@ -9,4 +9,9 @@
   };
 
   programs.dconf.enable = true;
+
+  services.udev.extraRules = ''
+    ACTION=="add", SUBSYSTEM=="backlight", KERNEL=="nvidia_wmi_ex_backlight", RUN+="/bin/chgrp video /sys/class/backlight/%k/brightness"
+    ACTION=="add", SUBSYSTEM=="backlight", KERNEL=="nvidia_wmi_ex_backlight", RUN+="/bin/chmod g+w /sys/class/backlight/%k/brightness"
+  '';
 }
