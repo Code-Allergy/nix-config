f7c89e1eb89d0f17097b14843c1f3a937ffb911a
Ryan Schaffer <ryan.schaffer@live.com>
2023-09-07 23:17:39 -0600

    added some vim plugins (coc)

--- home-manager/nvim.nix
+++ home-manager/nvim.nix
@@ -1,4 +1,4 @@
-{
+{pkgs, ...}: {
   #add to this later
   programs.neovim = {
     enable = true;
@@ -9,5 +9,32 @@
     coc.enable = true;
     coc.pluginConfig = "";
     coc.settings = {};
+
+    plugins = with pkgs.vimPlugins; [
+      vim-fugitive
+      nvim-lspconfig
+      # nodePackages.bash-language-server
+      # nodePackages.typescript-language-server
+      # nodePackages.vim-language-server
+      # nodePackages.yaml-language-server
+      # python311Packages.python-lsp-server
+
+      coc-python
+      coc-spell-checker
+      coc-sh
+      coc-rust-analyzer
+      coc-prettier
+      coc-nginx
+      coc-lua
+      coc-json
+      coc-toml
+      coc-java
+      coc-html
+      coc-css
+      coc-tsserver
+      coc-cmake
+      coc-docker
+      coc-git
+    ];
   };
 }
