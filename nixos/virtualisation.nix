1841a79e78ba29488fc51ccfc0a97b43901a2c40
Ryan Schaffer <ryan.schaffer@live.com>
2023-09-07 23:19:43 -0600

    switched to full OVMF package.

--- nixos/virtualisation.nix
+++ nixos/virtualisation.nix
@@ -1,4 +1,8 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  config,
+  ...
+}: {
   environment.systemPackages = with pkgs; [virt-manager];
 
   virtualisation = {
@@ -11,6 +15,7 @@
     libvirtd = {
       enable = true;
       qemu.ovmf.enable = true;
+      qemu.ovmf.packages = [pkgs.OVMFFull];
       qemu.swtpm.enable = true;
     };
     spiceUSBRedirection.enable = true;
@@ -40,6 +45,16 @@
       #     "--network=bridge"
       #   ];
       # };
+    };
+  };
+
+  environment.etc = {
+    "ovmf/edk2-x86_64-secure-code.fd" = {
+      source = config.virtualisation.libvirtd.qemu.package + "/share/qemu/edk2-x86_64-secure-code.fd";
+    };
+
+    "ovmf/edk2-i386-vars.fd" = {
+      source = config.virtualisation.libvirtd.qemu.package + "/share/qemu/edk2-i386-vars.fd";
     };
   };
 }
