bfb2284759847e7722e81eb8bbac45e0a2d2c3f7
Ryan Schaffer <ryan.schaffer@live.com>
2023-09-07 23:39:31 -0600

    added lightlocker

--- nixos/environments/qtile.nix
+++ nixos/environments/qtile.nix
@@ -1,4 +1,8 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  config,
+  ...
+}: {
   services.xserver = {
     enable = true;
     layout = "us";
@@ -9,7 +13,7 @@
     displayManager.lightdm.enable = true;
     displayManager.lightdm.greeters.enso.enable = true;
   };
-  programs.slock.enable = true;
+  programs.xss-lock.enable = true;
 
   programs.dconf.enable = true;
 
@@ -17,4 +21,8 @@
     ACTION=="add", SUBSYSTEM=="backlight", KERNEL=="nvidia_wmi_ex_backlight", RUN+="${pkgs.coreutils}/bin/chgrp video /sys/class/backlight/%k/brightness"
     ACTION=="add", SUBSYSTEM=="backlight", KERNEL=="nvidia_wmi_ex_backlight", RUN+="${pkgs.coreutils}/bin/chmod g+w /sys/class/backlight/%k/brightness"
   '';
+
+  environment.systemPackages = [
+    pkgs.lightlocker
+  ];
 }
