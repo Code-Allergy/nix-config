e797abe19d77fee38c3de42d4d263436ad1c2d1e
Ryan Schaffer <ryan.schaffer@live.com>
2024-09-04 00:19:27 -0600

    added wireplumber

--- nixos/hardware/audio.nix
+++ nixos/hardware/audio.nix
@@ -10,26 +10,14 @@
     playerctl
   ];
 
-  # security.rtkit.enable = true;
-  # services.pipewire = {
-  #   enable = true;
-  #   pulse.enable = true;
-  #   alsa = {
-  #     enable = true;
-  #     support32Bit = true;
-  #   };
-  # };
-
-  # sound.enable = true;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
+    wireplumber = {
+      enable = true;
+    };
   };
-
-  # sound.mediaKeys.enable = true;
 }
