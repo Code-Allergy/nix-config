7f86461e2e1cf29d41f6ad7afe7c6f8b9d0fb22c
Ryan Schaffer <rys686@mail.usask.ca>
2025-11-04 15:28:57 -0600

    more deprecated config updates

--- home/ryan/keyring/default.nix
+++ home/ryan/keyring/default.nix
@@ -1,29 +1,32 @@
-{pkgs, ...}: {
+{ pkgs, ... }:
+{
   # gnome-keyring
   services.gnome-keyring = {
     enable = true;
-    components = ["pkcs11" "secrets" "ssh"];
+    components = [
+      "pkcs11"
+      "secrets"
+      "ssh"
+    ];
   };
 
   systemd.user.services.keyring-daemon = {
     Unit = {
       Description = "GNOME Keyring daemon";
-      PartOf = ["graphical-session.target"];
+      PartOf = [ "graphical-session.target" ];
     };
     Service = {
       ExecStart = "${pkgs.gnome-keyring}/bin/gnome-keyring-daemon --foreground --components=secrets,ssh,pkcs11";
       Restart = "on-failure";
     };
     Install = {
-      WantedBy = ["graphical-session.target"];
+      WantedBy = [ "graphical-session.target" ];
     };
   };
 
   programs.gpg = {
     enable = true;
   };
-
-  programs.ssh.addKeysToAgent = "yes";
 
   services.gpg-agent = {
     enable = true;
