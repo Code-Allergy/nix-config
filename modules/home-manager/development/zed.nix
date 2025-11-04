cce67e7342b09da6e6f9a6d27692cc9bce41f45e
Ryan Schaffer <rys686@mail.usask.ca>
2025-11-04 15:42:42 -0600

    removed homemanager management of zed configs

--- modules/home-manager/development/zed.nix
+++ modules/home-manager/development/zed.nix
@@ -55,145 +55,145 @@ in
         # "http"
       ];
       installRemoteServer = true;
-      userSettings = {
-        assistant = {
-          default_model = {
-            provider = "zed.dev";
-            model = "claude-4-0-sonnet-latest";
-          };
-          version = "2";
-        };
-        agent = {
-          version = "2";
-          enabled = true;
-          button = true;
-          dock = "right";
-          default_width = 640;
-          default_height = 320;
-          default_model = {
-            provider = "zed.dev";
-            model = "claude-4-0-sonnet-latest";
-          };
-          editor_model = {
-            provider = "zed.dev";
-            model = "claude-4-0-sonnet-latest";
-          };
-          single_file_review = true;
-        };
-        # language_models = {
-        #   ollama = {
-        #     api_url = "http://localhost:11434";
-        #   };
-        # };
-        inlay_hints = {
-          enabled = true;
-        };
-        # features = {
-        #   edit_prediction_provider = "zed";
-        # };
-        terminal = {
-          env = {
-            EDITOR = "zeditor --wait";
-          };
-        };
-        file_types = {
-          Dockerfile = [
-            "Dockerfile"
-            "Dockerfile.*"
-          ];
-          JSON = [
-            "json"
-            "jsonc"
-            "*.code-snippets"
-          ];
-        };
-        file_scan_exclusions = [
-          # Zed defaults
-          "**/.git"
-          "**/.svn"
-          "**/.hg"
-          "**/CVS"
-          "**/.DS_Store"
-          "**/Thumbs.db"
-          "**/.classpath"
-          "**/.settings"
-        ];
-        telemetry = {
-          diagnostics = false;
-          metrics = false;
-        };
-        project_panel = {
-          button = true;
-          dock = "left";
-        };
-        outline_panel = {
-          dock = "right";
-        };
-        collaboration_panel = {
-          dock = "left";
-        };
-        notification_panel = {
-          dock = "left";
-        };
-        chat_panel = {
-          dock = "left";
-        };
-        lsp = {
-          rust-analyzer = {
-            initialization_options = {
-              rust = {
-                analyzerTargetDir = true;
-              };
-            };
-            binary = {
-              path_lookup = true;
-            };
-          };
-          pylsp = {
-            binary = {
-              path_lookup = true;
-            };
-          };
-          nil = {
-            binary = {
-              path_lookup = true;
-            };
-            initialization_options = {
-              formatting = {
-                command = [ "nixfmt" ];
-              };
-            };
-          };
-          nix = {
-            binary = {
-              path_lookup = true;
-            };
-          };
-          zls.binary.path_lookup = true;
-          wakatime = {
-            binary = {
-              path_lookup = true;
-              arguments = [
-                "--wakatime-cli"
-                "/etc/profiles/per-user/ryan/bin/wakatime-cli"
-              ];
-            };
-          };
-        };
-        languages = {
-          Nix = {
-            tab_size = 2;
-            language_servers = [
-              "nil"
-              "!nixd"
-            ];
-          };
-        };
-        autosave.after_delay.milliseconds = 1000;
-        load_direnv = "shell_hook";
-        ui_font_size = 16;
-        buffer_font_size = 14;
-      };
+      # userSettings = {
+      #   assistant = {
+      #     default_model = {
+      #       provider = "zed.dev";
+      #       model = "claude-4-0-sonnet-latest";
+      #     };
+      #     version = "2";
+      #   };
+      #   agent = {
+      #     version = "2";
+      #     enabled = true;
+      #     button = true;
+      #     dock = "right";
+      #     default_width = 640;
+      #     default_height = 320;
+      #     default_model = {
+      #       provider = "zed.dev";
+      #       model = "claude-4-0-sonnet-latest";
+      #     };
+      #     editor_model = {
+      #       provider = "zed.dev";
+      #       model = "claude-4-0-sonnet-latest";
+      #     };
+      #     single_file_review = true;
+      #   };
+      #   # language_models = {
+      #   #   ollama = {
+      #   #     api_url = "http://localhost:11434";
+      #   #   };
+      #   # };
+      #   inlay_hints = {
+      #     enabled = true;
+      #   };
+      #   # features = {
+      #   #   edit_prediction_provider = "zed";
+      #   # };
+      #   terminal = {
+      #     env = {
+      #       EDITOR = "zeditor --wait";
+      #     };
+      #   };
+      #   file_types = {
+      #     Dockerfile = [
+      #       "Dockerfile"
+      #       "Dockerfile.*"
+      #     ];
+      #     JSON = [
+      #       "json"
+      #       "jsonc"
+      #       "*.code-snippets"
+      #     ];
+      #   };
+      #   file_scan_exclusions = [
+      #     # Zed defaults
+      #     "**/.git"
+      #     "**/.svn"
+      #     "**/.hg"
+      #     "**/CVS"
+      #     "**/.DS_Store"
+      #     "**/Thumbs.db"
+      #     "**/.classpath"
+      #     "**/.settings"
+      #   ];
+      #   telemetry = {
+      #     diagnostics = false;
+      #     metrics = false;
+      #   };
+      #   project_panel = {
+      #     button = true;
+      #     dock = "left";
+      #   };
+      #   outline_panel = {
+      #     dock = "right";
+      #   };
+      #   collaboration_panel = {
+      #     dock = "left";
+      #   };
+      #   notification_panel = {
+      #     dock = "left";
+      #   };
+      #   chat_panel = {
+      #     dock = "left";
+      #   };
+      #   lsp = {
+      #     rust-analyzer = {
+      #       initialization_options = {
+      #         rust = {
+      #           analyzerTargetDir = true;
+      #         };
+      #       };
+      #       binary = {
+      #         path_lookup = true;
+      #       };
+      #     };
+      #     pylsp = {
+      #       binary = {
+      #         path_lookup = true;
+      #       };
+      #     };
+      #     nil = {
+      #       binary = {
+      #         path_lookup = true;
+      #       };
+      #       initialization_options = {
+      #         formatting = {
+      #           command = [ "nixfmt" ];
+      #         };
+      #       };
+      #     };
+      #     nix = {
+      #       binary = {
+      #         path_lookup = true;
+      #       };
+      #     };
+      #     zls.binary.path_lookup = true;
+      #     wakatime = {
+      #       binary = {
+      #         path_lookup = true;
+      #         arguments = [
+      #           "--wakatime-cli"
+      #           "/etc/profiles/per-user/ryan/bin/wakatime-cli"
+      #         ];
+      #       };
+      #     };
+      #   };
+      #   languages = {
+      #     Nix = {
+      #       tab_size = 2;
+      #       language_servers = [
+      #         "nil"
+      #         "!nixd"
+      #       ];
+      #     };
+      #   };
+      #   autosave.after_delay.milliseconds = 1000;
+      #   load_direnv = "shell_hook";
+      #   ui_font_size = 16;
+      #   buffer_font_size = 14;
+      # };
     };
   };
 }
