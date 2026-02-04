1dab1864b59b55cab1d2e4bc7953578af414a778
Ryan Schaffer <rys686@mail.usask.ca>
2026-02-04 05:18:58 -0600

    readded vpn module for tailscale

--- nixos/vpn.nix
+++ nixos/vpn.nix
@@ -1,12 +1,13 @@
 { pkgs, ... }:
 {
   # Enable Mullvad VPN
-  services.mullvad-vpn.enable = true;
+  # services.mullvad-vpn.enable = true;
   # `pkgs.mullvad` only provides the CLI tool, use `pkgs.mullvad-vpn` instead if you want to use the CLI and the GUI.
-  services.mullvad-vpn.package = pkgs.mullvad;
+  # services.mullvad-vpn.package = pkgs.mullvad;
   services.tailscale.enable = true;
+  
 
-  environment.systemPackages = with pkgs; [
-    mullvad-closest
-  ];
+  # environment.systemPackages = with pkgs; [
+    # mullvad-closest
+  # ];
 }
