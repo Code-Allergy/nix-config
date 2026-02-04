572acd1c645446bf7352fb37bbeae043f875df51
Ryan Schaffer <rys686@mail.usask.ca>
2026-02-04 05:25:21 -0600

    removed useless tailscale group

--- nixos/users/ryan/default.nix
+++ nixos/users/ryan/default.nix
@@ -17,7 +17,6 @@
       "kvm"
       "adbusers"
       "dialout"
-      "tailscale"
     ];
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE4N8Fiv6jdkPy8yMeE35HoFypjobZ2sq1I/G8iWui5T ryan@rys686@usask.ca"
