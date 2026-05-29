05bbbe323561f80fc1d83e905447125f531fc29f
Ryan Schaffer <rys686@mail.usask.ca>
2026-05-28 18:02:14 -0600

    flake updates and fixes

--- modules/home-manager/development/vscode.nix
+++ modules/home-manager/development/vscode.nix
@@ -29,8 +29,8 @@ in
           ms-vscode.cmake-tools
           ms-vscode.hexeditor
           ms-vscode-remote.remote-ssh
-          ms-python.python
-          ms-python.vscode-pylance
+          # ms-python.python
+          # ms-python.vscode-pylance
           file-icons.file-icons
           esbenp.prettier-vscode
           vscjava.vscode-maven
