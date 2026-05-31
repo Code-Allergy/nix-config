aabad4a915aecc00024573b94eff04dba661080d
Ryan Schaffer <rys686@mail.usask.ca>
2026-05-31 00:24:52 -0600

    refactor: blubbus changes

--- system/nixos/hosts/blubbus/system.nix
+++ system/nixos/hosts/blubbus/system.nix
@@ -1,118 +1,14 @@
+{ lib, ... }:
 {
-  lib,
-  pkgs,
-  ...
-}:
-{
-  imports = [
-    # Hardware config
-    ./fs.nix
-
-    ../../modules/virtualisation.nix
-
-    # Hardware
-    ../../hardware/audio.nix
-    ../../hardware/bluetooth.nix
-    ../../hardware/display.nix
-    ../../hardware/nvidia.nix
-    # ../../hardware/hibernate-after-sleep.nix
-    ../../modules/vpn.nix
-
-    # Fileserver mounts
-    ../../modules/samba-mounts.nix
-
-    ../../environments/plasma.nix
-    ../../environments/hyprland.nix
-  ];
+  imports = [ ./hardware.nix ];
 
   # New (2025) module configuration
   neer = {
     modules.gaming.enable = true;
   };
 
-  # Bootloader.
-  boot = {
-    loader.systemd-boot = {
-      enable = lib.mkForce false;
-      editor = true;
-    };
-    lanzaboote = {
-      enable = true;
-      pkiBundle = "/var/lib/sbctl";
-    };
-
-    plymouth = {
-      enable = true;
-      theme = "catppuccin-mocha";
-    };
-    consoleLogLevel = 0;
-    initrd.verbose = false;
-    initrd.systemd.enable = true;
-    kernelParams = [
-      "quiet"
-      "splash"
-      "boot.shell_on_fail"
-      "loglevel=3"
-      # "rd.systemd.show_status=false"
-
-      # hibernation
-      "nvidia.NVreg_UsePageAttributeTable=1"
-      "nvidia.NVreg_PreserveVideoMemoryAllocations=1"
-      "nvidia.NVreg_TemporaryFilePath=/var/tmp"
-
-      "acpi_backlight=nvidia_wmi_ec"
-    ];
-
-    initrd.availableKernelModules = [
-      "nvme"
-      "xhci_pci"
-      "ahci"
-      "usb_storage"
-      "sd_mod"
-    ];
-
-    initrd.luks.devices = {
-      root = {
-        device = "/dev/disk/by-uuid/d19873f6-caa4-43a2-8032-0b6cc8e72190";
-        preLVM = true;
-      };
-    };
-    loader.timeout = 0;
-
-    # Nvidia drivers often break on latest kernel.. use LTS instead.
-    kernelPackages = pkgs.linuxPackages;
-  };
-
-  networking.useDHCP = lib.mkDefault true;
-
-  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-
-  environment.systemPackages = with pkgs; [
-    # power management
-    powertop
-    brightnessctl
-
-    # Wifi applet
-    networkmanagerapplet
-  ];
-
-  powerManagement = {
-    enable = true;
-    powertop.enable = true;
-  };
-
-  # Laptop TLP battery saving config
-  services.power-profiles-daemon.enable = true;
-  services.auto-epp.enable = true;
-
-  # Enable acpi daemon so laptop close/open is responded to
-  services.acpid.enable = true;
-
-  # Display brightness
-  hardware.acpilight.enable = true;
-
-  # Network config
   networking = {
+    useDHCP = lib.mkDefault true;
     hostName = "blubbus";
     networkmanager = {
       enable = true;
@@ -137,8 +33,6 @@
       ];
     };
   };
-
-  services.fwupd.enable = true;
 
   # Lock on lid close
   services.logind.settings.Login = {
