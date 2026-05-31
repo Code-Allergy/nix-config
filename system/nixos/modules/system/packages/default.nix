9d2d0bd97360eb24249f87a6dbb0b82c8d87f30a
Ryan Schaffer <rys686@mail.usask.ca>
2026-05-31 01:09:56 -0600

    added general packages module

---
+++ system/nixos/modules/system/packages/default.nix
@@ -0,0 +1,10 @@
+{ pkgs, ... }:
+
+{
+  config = {
+    programs.zsh.enable = true;
+    environment.systemPackages = with pkgs; [
+      # Packages that I want installed at the system level, no matter the system
+    ];
+  };
+}
